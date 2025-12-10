40d2408d7306a94542e528335b985d54e63b73d6
ruzbyte <arthuraktamirov@gmail.com>
2025-12-10 14:41:52 +0100

    feat : add fastfetch refactor : hyprland config improvements

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+# source /usr/share/cachyos-fish-config/cachyos-config.fish
 source (/usr/bin/starship init fish --print-full-init | psub)
 # overwrite greeting
 # potentially disabling fastfetch
