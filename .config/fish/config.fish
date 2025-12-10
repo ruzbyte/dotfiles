387874038f3e99f9325b55e98e6595f63eecdf80
ruzbyte <arthuraktamirov@gmail.com>
2025-12-10 14:43:02 +0100

    fix: move fastfetch config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 # source /usr/share/cachyos-fish-config/cachyos-config.fish
 source (/usr/bin/starship init fish --print-full-init | psub)
+fastfetch
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
