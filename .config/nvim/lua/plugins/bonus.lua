3efc73dcc01e2e55e73be4b81a981920045890e2
ruzbyte <arthuraktamirov@gmail.com>
2025-12-10 21:51:06 +0100

    feat: add nvim

---
+++ .config/nvim/lua/plugins/bonus.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "tpope/vim-fugitive",
+  },
+  {
+    "brenoprata10/nvim-highlight-colors",
+    config = function()
+      require("nvim-highlight-colors").setup({})
+    end,
+  },
+}
