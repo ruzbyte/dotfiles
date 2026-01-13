07695ac1698ad42dc377afbaaa8af4cc97627e5d
ruzbyte <arthuraktamirov@gmail.com>
2026-01-13 12:06:47 +0100

    chore: add noctalia settings to dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ export PATH="$PATH:$ANDROID_HOME/emulator"
 export PATH="$PATH:$ANDROID_HOME/platform-tools"
 export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
 
+set -U fish_greeting
+
 source (/usr/bin/starship init fish --print-full-init | psub)
 fastfetch
-
